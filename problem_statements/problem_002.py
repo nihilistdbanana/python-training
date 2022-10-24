4b874cc3cc58b8e60b6929d261cfb20211afafae
Shashank Gupta <lbsrex@gmail.com>
2022-10-24 18:53:25 +0530

    Create problem_002.py

---
+++ problem_statements/problem_002.py
@@ -0,0 +1,10 @@
+# print sum of numbers from 1 to n, where you take n as input from user
+
+
+# print sum of squares of numbers from 1 to n, where you take n as input from user
+
+
+# print result of adding all even numbers and subtracting all odd numbers from 1 to n, where you take n as input from user
+
+
+# (Optional) now try to solve all problems above without using a loop
