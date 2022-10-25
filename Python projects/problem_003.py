5c3aab6539742bd78ed43067cbb8f6a401378c0a
Ajinkya Chavan <114818122+Aju2426@users.noreply.github.com>
2022-10-25 16:27:56 +0530

    Update problem_003.py

--- Python projects/problem_003.py
+++ Python projects/problem_003.py
@@ -1,32 +1,30 @@
 #print the following pattern
-for i in range(1,7):             # i for row reference
-    for j in range(1,i+1):       # j for a column reference
+n=6
+for i in range(n):              # i for row reference
+    for j in range(i+1):        # j for a column reference
         print("*",end="")
-    print("\n")                 #Print stars in a new line
+    print()                     #Print stars in a new line
 
-# print the following pattern
+#print the following pattern
+n=6
+for i in range(n):
+    for j in range(n-i-1):      # J for a Space reference
+        print(' ',end="")
+    for j in range(2*i+1):
+        print('*',end="")
+    print()
 
-for i in range(1,7):
-    for k in range(1,7-i):     # K for a Space reference
-        print(" ",end="")
-    for j in range(1,(2*i-1)+1):
-        print("*",end="")
-    print("\n")
-
-# print the following pattern
-
-for i in range(1,7):
-    for k in range(1,7-i):
-        print(" ",end="")
-    for j in range(1,(2*i-1)+1):
-        print("*",end="")
-    print("\n")
-for i in range(5,0,-1):
-    for k in range(1,5-i):
-        print(" ",end="")
-    for j in range(1,(2*i-1)+1):
-        print("*",end="")
-    print("\n")
-
-
-
+#print the following pattern
+n=6
+for i in range(n):
+    for j in range(n-i-1):
+        print(' ',end="")
+    for j in range(2*i+1):
+        print('*',end="")
+    print()
+for i in range(n):
+    for j in range(i):
+        print(' ',end="")
+    for j in range(2*(n-i)-1):
+        print('*',end="")
+    print()
