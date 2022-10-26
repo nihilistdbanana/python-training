c0505a600788041e275de6e8aeca37f9e90cbefe
gazsgithub <115701511+gazsgithub@users.noreply.github.com>
2022-10-26 09:50:22 +0100

    Add 04_06_challenge_wip.py  solution

---
+++ ryanmitchell/04_06_challenge_wip.py
@@ -0,0 +1,64 @@
+import os
+import time
+from termcolor import colored
+import math 
+from collections import defaultdict 
+
+
+class Canvas:
+    def __init__(self, width, height):
+        self._x = width
+        self._y = height
+        self._canvas = [[' ' for y in range(self._y)] for x in range(self._x)]
+
+    def hitsWall(self, point):
+        return round(point[0]) < 0 or round(point[0]) >= self._x or round(point[1]) < 0 or round(point[1]) >= self._y
+
+    def setPos(self, pos, mark):
+        self._canvas[round(pos[0])][round(pos[1])] = mark
+
+    def clear(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def print(self):
+        self.clear()
+        for y in range(self._y):
+            print(' '.join([col[y] for col in self._canvas]))
+
+class TerminalScribe:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.trail = '.'
+        self.mark = '*'
+        self.framerate = 0.05
+        self.pos = [0, 0]
+        self.direction = [0, 1]
+
+    def setDegrees(self, degrees):
+        radians = math.radians(degrees)
+        self.direction = [math.sin(radians), -math.cos(radians)]
+
+    def forward(self):
+        pos = [self.pos[0] + self.direction[0], self.pos[1] + self.direction[1]]
+        if not self.canvas.hitsWall(pos):
+            self.draw(pos)
+
+    def draw(self, pos):
+        self.canvas.setPos(self.pos, self.trail)
+        self.pos = pos
+        self.canvas.setPos(self.pos, colored(self.mark, 'red'))
+        self.canvas.print()
+        time.sleep(self.framerate)
+
+    def setAngleDistance(self,angle,distance):
+        self.setDegrees(angle)
+        print(angle)
+        print(distance)
+        for i in range(distance):
+            print(distance)
+            
+canvas = Canvas(30, 30)
+scribe = TerminalScribe(canvas)
+scribe.setDegrees(145)
+for i in range(30):
+    scribe.forward()
