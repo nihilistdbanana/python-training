980bf65540a8076d7165a572c520d9dc386dc8d1
Rage-cmd <55309213+Rage-cmd@users.noreply.github.com>
2023-10-22 16:23:20 +0530

    added the solution for part 2 of proplem 2

--- problem_statements/problem_002.py
+++ problem_statements/problem_002.py
@@ -2,9 +2,31 @@
 
 
 # print sum of squares of numbers from 1 to n, where you take n as input from user
+# n = int(input("Enter a number"))
 
+# if(n <= 0):
+#     print("Please enter a positive number ")
+#     quit()
+
+# ans = 0
+# for i in range(1,n+1):
+#     ans += i**2
+
+# print("The sum of squares of numbers from 1 to ", n, " is ", ans)
 
 # print result of adding all even numbers and subtracting all odd numbers from 1 to n, where you take n as input from user
 
 
 # (Optional) now try to solve all problems above without using a loop
+
+#problem 2
+
+# the sum of squares of numbers from 1 to n is equal to n(n+1)(2n+1)/6
+n = int(input("Enter a number"))
+
+if(n <= 0):
+    print("Please enter a positive number ")
+    quit()
+
+ans = int(n*(n+1)*(2*n+1)/6)
+print("The sum of squares of numbers from 1 to ", n, " is ", ans)
